9d5029c0813a843c5e4ebf60a584383efab4ef4f
shenzhiyongzhe <67083540+shenzhiyongzhe@users.noreply.github.com>
2023-07-23 10:53:50 +0800

    Add files via upload

---
+++ 自动答题插件/SingleChoice.js
@@ -0,0 +1,20 @@
+// 单选题题库
+const SingleChoice = [
+  {
+    title: "单选题1",
+    options: ["选项 1", "选项 2", "选项 3", "选项 4"],
+    rightAnswer: 0,
+  },
+  {
+    title: "单选题2",
+    options: ["选项 1", "选项 2", "选项 3", "选项 4"],
+    rightAnswer: 2,
+  },
+  {
+    title: "单选题3",
+    options: ["选项 1", "选项 2", "选项 3", "选项 4"],
+    rightAnswer: 3,
+  },
+];
+
+export default SingleChoice;
