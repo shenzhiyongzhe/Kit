94de55be390aebcd1b16ac8f47405bf8c10bf1b4
shenzhiyongzhe <67083540+shenzhiyongzhe@users.noreply.github.com>
2023-07-23 10:56:30 +0800

    Add files via upload

---
+++ 自动答题插件/TestDatabase/Judgment.js
@@ -0,0 +1,20 @@
+//判断题题库
+const Judgment = [
+  {
+    title: "判断题1",
+    options: ["选项 1", "选项 2"],
+    rightAnswer: 1,
+  },
+  {
+    title: "判断题2",
+    options: ["选项 1", "选项 2"],
+    rightAnswer: 1,
+  },
+  {
+    title: "判断题3",
+    options: ["选项 1", "选项 2"],
+    rightAnswer: 1,
+  },
+];
+
+export default Judgment;
